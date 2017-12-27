d5be74c2ebfaca5f154815b5917851d5ab2fc52a
rjklein <rj.klein@comcast.net>
2017-12-27 14:07:21 -0600

    ENV fixes

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const routes = require('./server/routes/index');
 
 const app = express();
 
+require('dotenv').config();
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
